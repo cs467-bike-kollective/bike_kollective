39fe6b8447e1197481d53e6f24ddc90eac578f85
kallytang <tangka@oregonstate.edu>
2022-10-16 20:25:00 -0700

    add changes to CameraActivity

--- app/src/main/java/com/example/bikekollective/CameraActivity.kt
+++ app/src/main/java/com/example/bikekollective/CameraActivity.kt
@@ -1,30 +1,40 @@
 package com.example.bikekollective
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.camera.core.ImageCapture
+import androidx.core.content.ContextCompat
+import com.example.bikekollective.databinding.ActivityMainBinding
+import java.io.File
+import java.util.concurrent.ExecutorService
 
+typealias LumaListener = (luma: Double) -> Unit
 class CameraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var imageCapture: ImageCapture? = null
+    private lateinit var cameraExecutor: ExecutorService
+    private var destination: Int? = null
+    private lateinit var outPutDirectory: File
+
+    companion object {
+        private const val TAG = "CameraXInfo"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 66
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private val FROM_ADD_BIKE_CODE = 2000
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        destination = intent.getIntExtra("identifier", 23)
 
     }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
-
 
 }
