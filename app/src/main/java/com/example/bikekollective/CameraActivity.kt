e0a8b0540f36f9951bbf73cfc70ce5addc1c1756
kallytang <tangka@oregonstate.edu>
2022-10-16 17:52:59 -0700

    add dependencies for CameraX

--- app/src/main/java/com/example/bikekollective/CameraActivity.kt
+++ app/src/main/java/com/example/bikekollective/CameraActivity.kt
@@ -2,12 +2,28 @@ package com.example.bikekollective
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 
 
