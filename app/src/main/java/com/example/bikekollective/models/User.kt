d5b8035bc49dcd663a4d5b503ba81c1a70b347ec
kallytang <tangka@oregonstate.edu>
2022-10-18 19:47:20 -0700

    add property borrowed_bike

--- app/src/main/java/com/example/bikekollective/models/User.kt
+++ app/src/main/java/com/example/bikekollective/models/User.kt
@@ -6,6 +6,7 @@ data class User(
     var email: String? = "",
     @get:PropertyName("display_name") @set:PropertyName("display_name") var displayName: String? = "",
     @get:PropertyName("is_flagged") @set:PropertyName("is_flagged") var isFlagged: Boolean? = null,
+    @get:PropertyName("borrowed_bike") @set:PropertyName("borrowed_bike") var borrowedBike: String? = null,
 
 )
 
