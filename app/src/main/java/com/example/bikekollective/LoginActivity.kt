f8f5c329c969c934586968ba04d09a5606d17730
kallytang <tangka@oregonstate.edu>
2022-10-18 22:42:22 -0700

    add section of code to handle sign in credential

--- app/src/main/java/com/example/bikekollective/LoginActivity.kt
+++ app/src/main/java/com/example/bikekollective/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.bikekollective
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.bikekollective.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -15,11 +23,39 @@ import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
 
+    private var showOneTapUI = true
+    companion object {
+        private val TAG = "LoginActivity"
+    }
 
+    //set up function to handle result from google Auth sign in
+    val startLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            try {
+                val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                val idToken = credential.googleIdToken
+                when {
+                    idToken != null -> {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        Log.d(TAG, "Got ID token.")
+                    }
+                    else -> {
+                        // Shouldn't happen.
+                        Log.d(TAG, "No ID token!")
+                    }
+                }
+            } catch (e: ApiException) {
+                // ...
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,6 +65,54 @@ class LoginActivity : AppCompatActivity() {
         // initialize Firebase Auth
         auth = Firebase.auth
 
+        // set up oneTapClient for Google Authentication
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+
 
     }
+
+
+
+
+
 }
+
+//override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//    super.onActivityResult(requestCode, resultCode, data)
+//
+//    when (requestCode) {
+//        REQ_ONE_TAP -> {
+//            try {
+//                val credential = oneTapClient.getSignInCredentialFromIntent(data)
+//                val idToken = credential.googleIdToken
+//                when {
+//                    idToken != null -> {
+//                        // Got an ID token from Google. Use it to authenticate
+//                        // with Firebase.
+//                        Log.d(TAG, "Got ID token.")
+//                    }
+//                    else -> {
+//                        // Shouldn't happen.
+//                        Log.d(TAG, "No ID token!")
+//                    }
+//                }
+//            } catch (e: ApiException) {
+//                // ...
+//            }
+//        }
+//    }
