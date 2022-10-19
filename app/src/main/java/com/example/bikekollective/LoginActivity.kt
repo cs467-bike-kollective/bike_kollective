8d7db2f671574cc75300dfa24ad8a5e87c7737a5
kallytang <tangka@oregonstate.edu>
2022-10-18 19:04:51 -0700

    add code in LoginActivity

--- app/src/main/java/com/example/bikekollective/LoginActivity.kt
+++ app/src/main/java/com/example/bikekollective/LoginActivity.kt
@@ -1,17 +1,24 @@
 package com.example.bikekollective
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.bikekollective.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +26,9 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // initialize Firebase Auth
+        auth = Firebase.auth
 
 
     }
-
-
 }
