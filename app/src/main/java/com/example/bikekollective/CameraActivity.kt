151a9ef66627f16584bcc4b7f94c3c1ea8fbd9bf
kallytang <tangka@oregonstate.edu>
2022-10-28 22:54:20 -0700

    remove audio permissions, start set up of cameraActivity

--- app/src/main/java/com/example/bikekollective/CameraActivity.kt
+++ app/src/main/java/com/example/bikekollective/CameraActivity.kt
@@ -1,18 +1,30 @@
 package com.example.bikekollective
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
 
 import androidx.core.content.ContextCompat
 import com.example.bikekollective.databinding.ActivityCameraBinding
 import com.example.bikekollective.databinding.ActivityMainBinding
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 typealias LumaListener = (luma: Double) -> Unit
 class CameraActivity : AppCompatActivity() {
@@ -44,6 +56,124 @@ class CameraActivity : AppCompatActivity() {
         binding.ivExitCamera.setOnClickListener {
             startActivity(Intent(this, CreateBikeActivity::class.java))
         }
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
+
+
+    private fun captureVideo() {}
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
 }
--- app/src/main/java/com/example/bikekollective/CreateBikeActivity.kt
+++ app/src/main/java/com/example/bikekollective/CreateBikeActivity.kt
@@ -1,16 +1,26 @@
 package com.example.bikekollective
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Camera
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.bikekollective.databinding.ActivityCameraBinding
 import com.example.bikekollective.databinding.ActivityCreateBikeBinding
 
 class CreateBikeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateBikeBinding
+    companion object {
+        private const val TAG = "CameraXInfo"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityCreateBikeBinding.inflate(layoutInflater)
         val view = binding.root
@@ -19,8 +29,29 @@ class CreateBikeActivity : AppCompatActivity() {
         // hide action bar
         supportActionBar?.hide();
 
+//        https://stackoverflow.com/a/66096319
+
         binding.bikeImage.setOnClickListener {
             startActivity(Intent(this, CameraActivity::class.java))
+        }
+        binding.submitFormButton.setOnClickListener {
+            // prevent user from submitting form multiple times
+            binding.submitFormButton.isEnabled = false
+
+            val descriptionInput = binding.bikeDescription.text.toString()
+            val combinationInput = binding.bikeLockCombination.text.toString()
+            //check if description is entered
+            if (descriptionInput.isEmpty()){
+                binding.bikeDescription.hint = "Add Description"
+                binding.bikeDescription.setHintTextColor(Color.RED)
+            }
+            // check if the combination entered is not empty
+            if (combinationInput.isEmpty()){
+                binding.bikeLockCombination.hint = "Add Combination"
+                binding.bikeLockCombination.setHintTextColor(Color.RED)
+            }
+
+            Log.i(TAG, "$descriptionInput $combinationInput")
         }
     }
 
