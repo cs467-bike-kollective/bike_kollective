fd8453e2e9aaa93eed697af7b5e1b90dfd23f3b2
Mengmeng Tang <francest221@gmail.com>
2022-10-31 19:00:01 -0700

    clean up home frag code

--- app/src/main/java/com/example/bikekollective/MainActivity.kt
+++ app/src/main/java/com/example/bikekollective/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
 //        setSupportActionBar(binding.toolbar)
 
 //        val navController = findNavController(R.id.nav_host_fragment_content_main)
