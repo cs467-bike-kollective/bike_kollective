381d1777d9ee23f893c9495051be54fd56162ca2
Mengmeng Tang <francest221@gmail.com>
2022-11-09 16:51:04 -0800

    test commit on new branch

--- app/src/main/java/com/example/bikekollective/SearchFragment.kt
+++ app/src/main/java/com/example/bikekollective/SearchFragment.kt
@@ -20,6 +20,8 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
 
+        // test commit
+
     }
 
 }
