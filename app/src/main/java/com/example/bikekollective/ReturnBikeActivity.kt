8c9be71f233042c70831d8629b4dd0e448b4ede7
Kharissa <kharissa@users.noreply.github.com>
2022-11-14 17:12:55 -0800

    update xml layout for return bike activity

---
+++ app/src/main/java/com/example/bikekollective/ReturnBikeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bikekollective
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReturnBikeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_return_bike)
+    }
+}
