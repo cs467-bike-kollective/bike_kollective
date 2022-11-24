7ec9b633086d341c863d22bb7b5c9231a2421da4
Kharissa <kharissa@users.noreply.github.com>
2022-11-24 00:19:54 -0800

    fix ratingbar bug

--- app/src/main/java/com/example/bikekollective/HomeFragment.kt
+++ app/src/main/java/com/example/bikekollective/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.bikekollective.databinding.FragmentHomeBinding
 import com.example.bikekollective.databinding.FragmentUserBikeListBinding
 import com.firebase.ui.auth.AuthUI
@@ -21,6 +22,7 @@ class HomeFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
+
     }
 
     override fun onCreateView(
@@ -41,6 +43,9 @@ class HomeFragment : Fragment() {
                         requireActivity().finish()
                     }
             }
+        }
+        binding.returnBikeBut.setOnClickListener {
+            startActivity(Intent(context, ReturnBikeActivity::class.java))
         }
         return binding.root
 
--- app/src/main/java/com/example/bikekollective/ReturnBikeActivity.kt
+++ app/src/main/java/com/example/bikekollective/ReturnBikeActivity.kt
@@ -5,36 +5,36 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RatingBar
+import android.widget.RatingBar.OnRatingBarChangeListener
+import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.bikekollective.databinding.ActivityReturnBikeBinding
+import com.example.bikekollective.databinding.FragmentHomeBinding
 
 class ReturnBikeActivity : AppCompatActivity() {
+    lateinit var ratingBar: RatingBar
+    lateinit var button: Button
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_return_bike)
+        ratingBar = findViewById(R.id.ratingBar)
+        button = findViewById(R.id.button_rate)
+        textView = findViewById<TextView>(R.id.rating)
+        ratingBar.numStars = 5
+        ratingBar.onRatingBarChangeListener =
+            OnRatingBarChangeListener {
+                    _, rating, _ ->
+                textView.text = "" + rating.toInt()+ "/5"
 
-        val rBar = RatingBar(this)
-        val layoutParams = ConstraintLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT)
-        rBar.rating = 4.5F
-        rBar.stepSize = 0.5.toFloat()
-        rBar.numStars = 5
-        rBar.layoutParams = layoutParams
-
-        val button = findViewById<Button>(R.id.button_rate)
-        ConstraintLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT)
-
-        val linearLayout = findViewById<ConstraintLayout>(R.id.container)
-        linearLayout?.addView(rBar)
-        linearLayout?.addView(button)
-
-        button?.setOnClickListener {
-            val msg = rBar.rating.toString()
-            Toast.makeText(this@ReturnBikeActivity, "Given Rating: $msg",
-                Toast.LENGTH_SHORT).show()
+            }
+        button.setOnClickListener {
+            Toast.makeText(
+                this@ReturnBikeActivity, "Stars: " +
+                        ratingBar.rating.toInt(), Toast.LENGTH_SHORT
+            ).show()
         }
+
     }
 }
